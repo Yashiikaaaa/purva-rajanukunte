264b1e1b5098abb38a50092b51f468e59c731813
yashiikaaaa <yashika@truestate.in>
2025-10-03 11:29:55 +0530

    button

--- src/components/button/buttonMain.jsx
+++ src/components/button/buttonMain.jsx
@@ -5,7 +5,7 @@ const Button = ({ text, onClick, className, showArrow = true }) => {
   return (
     // Button container with applied styles
     <div
-      className={`flex items-center px-10 py-4 w-fit bg-[#6a500d] text-white cursor-pointer 
+      className={`flex items-center px-10 py-4 w-fit bg-[#675f4a] text-white cursor-pointer 
                   font-semibold text-xs md:text-sm uppercase whitespace-nowrap gap-10
                   transition-all duration-300 ease-in-out 
                   hover:bg-white hover:text-black ${className} font-body`} // Combining dynamic and static class names
